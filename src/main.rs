26009b539225f722d89236ce776e6c5f7a3a09ca
decker <mdatmosci@gmail.com>
2024-11-03 08:17:42 -0600

    Generate a secret random number, output to screen for testing

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,17 @@
 use std::io;
+use rand::Rng;
 
 fn main() {
-    let max_guess = 9;
-    let min_guess = 0;
+    let max_guess = 100; //outside of tutorial scope at the moment
+    let min_guess = 1; //but figures I'd want to limit range at a later point
 
     println!("Guess the number!");
 
-    println!("Please input your guess between {}-{} inclusive",min_guess,max_guess);
+    let secret_number = rand::thread_rng().gen_range(min_guess..=max_guess);
+
+    println!("The secret number is {}",secret_number);
+
+    println!("Please input your guess between {min_guess}-{} inclusive",max_guess);
 
     let mut guess = String::new();
 
