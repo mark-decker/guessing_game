67e42c9c598a8173123bd66d9f0891600e047949
decker <mdatmosci@gmail.com>
2024-11-04 07:55:43 -0600

    add loop to allow multiple guesses.  shadow guess as u32 to allow using match for comparison

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::io;
 use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
     let max_guess = 100; //outside of tutorial scope at the moment
@@ -9,16 +10,32 @@ fn main() {
 
     let secret_number = rand::thread_rng().gen_range(min_guess..=max_guess);
 
-    println!("The secret number is {}",secret_number);
-
-    println!("Please input your guess between {min_guess}-{} inclusive",max_guess);
-
+    loop {
+    println!("Please input your guess between {min_guess}-{max_guess} inclusive");
+    
     let mut guess = String::new();
+    
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+    
+        //let guess: u32 = guess.trim().parse().expect("Please type a number!"); //Crashed on error
+        let guess: u32 = match guess.trim().parse() {   //Handle the error
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        println!("You guessed: {}", guess);
+    
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
 
-    println!("You guessed: {}", guess);
+    }
 
 }
